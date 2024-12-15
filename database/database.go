3166759cb5d750ca93a5855ed8307399d74562c5
vicentebravocabezas <26090986+vicentebravocabezas@users.noreply.github.com>
2024-12-15 10:49:54 -0500

    codigo para tests

--- database/database.go
+++ database/database.go
@@ -9,18 +9,25 @@ import (
 )
 
 var (
-	once sync.Once
-	db   *sql.DB
+	once   sync.Once
+	db     *sql.DB
+	dbFile = "file:./database/local/db.sqlite3"
 )
 
-func init() {
-	DB()
+// esta función solo debe usarse para testing, al llamarse, se cerrara cualquier conexión a una DB
+// y los siguientes llamados a DB() utilizaran la nueva ruta de la base de datos
+func SetDBFile(dbPath string) {
+	if db != nil {
+		db.Close()
+	}
+	dbFile = dbPath
+	once = sync.Once{}
 }
 
 func DB() *sql.DB {
 	once.Do(func() {
 		var err error
-		db, err = sql.Open("libsql", "file:./database/local/db.sqlite3")
+		db, err = sql.Open("libsql", dbFile)
 		if err != nil {
 			log.Fatal(err)
 		}
